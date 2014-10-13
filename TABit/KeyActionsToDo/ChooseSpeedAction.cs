a8d5a1e0b39a460c478a9d6a53b0524c3c29fe97
Basraga <sebastian.kasper96@gmx.at>
2014-10-13 15:50:53 +0200

    Added KeyActions
    Hab ma des Zeugs geändert. Komisch is des abe au. Des Zeugs. Jetz gibt's
    da n Interface und son Namespace mit Actions.

---
+++ TABit/KeyActionsToDo/ChooseSpeedAction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TABit.Actions
+{
+    public class ChooseSpeedAction : KeyAction
+    {
+
+        public ChooseSpeedAction() 
+        {
+        
+        }
+
+
+        public void doKeyAction(TextBox box)
+        {
+        }
+    }
+}
---
+++ TABit/KeyActionsToDo/OpenMenueAction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TABit.Actions
+{
+    public class OpenMenueAction : KeyAction
+    {
+           
+        public OpenMenueAction() 
+        {
+        
+        }
+
+
+        public void doKeyAction(TextBox box)
+        {
+        }
+    }
+}
