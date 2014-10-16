45e25bf086f77b98e910ac46483ad09d69780295
Basraga <sebastian.kasper96@gmx.at>
2014-10-16 12:11:31 +0200

    Added WriteNoteAction Function
    Da gibts nu ne Funktion in de WriteNoteAction.
    NumPad, x un die Ziffern obe schreiben nu ihre Wert.
    Komisch des C#.

--- TABit/KeyActionsToDo/WriteNoteAction.cs
+++ TABit/KeyActionsToDo/WriteNoteAction.cs
@@ -9,14 +9,25 @@ namespace TABit.Actions
 {
     public class WriteNoteAction : KeyAction
     {
+        private int ToWrite;
+
         public WriteNoteAction(int ToWrite) 
         {
-        
+            this.ToWrite = ToWrite;
         }
 
 
         public void doKeyAction(TextBox box)
         {
+            
+            string Writting = ToWrite.ToString();
+
+            
+            KeysConverter kc = new KeysConverter();
+
+
+                
+            box.Text = box.Text.Insert(box.SelectionStart, kc.ConvertToString(ToWrite));
         }
     }
 }
