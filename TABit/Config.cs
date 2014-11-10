6d8241285fc5d48b540950edf75ce15c37a6352b
picturestone <lukas.bildstein@outlook.com>
2014-11-10 13:17:32 +0100

    Added static Config Class
    id iz god furr da furrda porgramming

---
+++ TABit/Config.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TABit
+{
+    static class Config
+    {
+        public static int get_lines_between_blocks()
+        {
+            string user_lines_between_blocks = TABit.Properties.Settings.Default.SLinesUser;
+            if (user_lines_between_blocks == "")
+            {
+                return Convert.ToInt16(TABit.Properties.Settings.Default.SLinesDefault);
+            }
+            return Convert.ToInt16(user_lines_between_blocks);
+        }
+    }
+}
